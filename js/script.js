4fc04816e8fb4ecef9986adc7824fa66b4854cf5
Wagner Taiatella <wtaiatella@gmail.com>
2021-11-07 20:55:12 -0400

    First Commit - portifolio page

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+if(window.SimpleSlide){
+
+   new SimpleSlide({
+      slide: 'quote', // nome do atributo data-slide="principal"
+      //nav: true, // se deve ou não mostrar a navegação
+      auto: true, // se o slide deve passar automaticamente
+      time: 5000, // tempo de transição dos slides
+      pauseOnHover: true, // pausa a transição automática
+   });
+
+   new SimpleSlide({
+      slide: 'portifolio', // nome do atributo data-slide="principal"
+      nav: true, // se deve ou não mostrar a navegação
+      auto: true, // se o slide deve passar automaticamente
+      time: 5000, // tempo de transição dos slides
+      pauseOnHover: true, // pausa a transição automática
+   });
+
+}
+
+if(window.SimpleAnime){
+   new SimpleAnime();
+}
+
+if(window.SimpleForm){
+new SimpleForm({
+   form: ".formphp", // seletor do formulário
+   button: "#send", // seletor do botão
+   erro: "<div id='form-erro'><h2>Sent error!</h2><p>Sorry, anerror has occurred, send an email to wtaiatella@gmail.com.</p></div>", // mensagem de erro
+   sucesso: "<div id='form-success'><h2>Form sent successfully!!</h2><p>Soon I will get in contact with you.</p> <p>Thanks!</p></div>", // mensagem de sucesso
+ });
+
+}
---
+++ send.php
@@ -0,0 +1,41 @@
+<?php
+
+require './sendgrid-php/sendgrid-php.php';
+
+$email_site = 'wtaiatella@gmail.com';
+$nome_site = "Wagner Taiatella's Resume";
+
+$email_user = $_POST['email'];
+$nome_user = $_POST['name'];
+
+$body_content = '';
+
+foreach ($_POST as $field => $value) {
+    if (
+        $field !== 'leaveblank' &&
+        $field !== 'dontchange' &&
+        $field !== 'send'
+    ) {
+        $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+        $body_content .= "$field: $value \n";
+    }
+}
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject('Formulário Bikcraft');
+$email->addContent('text/plain', $body_content);
+
+$sendgrid = new \SendGrid('minhachave***********');
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ' . $e->getMessage() . "\n";
+}
